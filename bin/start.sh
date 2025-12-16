a6cb8447129ef1a91d1eb1472e5337c14cf0a7d1
Khudyakov Artem <artem170289@mail.ru>
2025-12-16 18:12:46 +0300

    with run-3

--- bin/start.sh
+++ bin/start.sh
@@ -2,9 +2,9 @@
 
 # Запускаем FastAPI в фоновом режиме
 echo "Starting FastAPI application..."
-make start &
+make run &
 # Ждем немного, чтобы FastAPI успел запуститься
-while ! curl -s http://127.0.0.1:8081/health >/dev/null; do
+while ! curl -s http://127.0.0.1:8080/health >/dev/null; do
   sleep 1
 done
 
